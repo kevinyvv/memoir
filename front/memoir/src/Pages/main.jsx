40c7b4bc440505a9c0cbf9152e86b24ab5930aab
Kevin Wu <kevin31422005@gmail.com>
2024-01-27 07:29:03 -0500

    Update main.jsx

--- front/memoir/src/Pages/main.jsx
+++ front/memoir/src/Pages/main.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
 
 const Main = () => {
+
+  const handleClick = () => {
+
+  }
+
+
   return (
     //change bg later
     <div className='h-screen w-screen'>
-      <button className='absolute top-5 right-5 btn glass px-8'> Login. </button>
+      <button className='absolute top-5 right-5 btn glass px-8'
+      onClick={handleClick}> Login. </button>
 
-      <p className='font-bold text-3xl'>Hello
-      </p>
+      <div className='relative top-1/3 text-center'>
+        <h1 className='font-bold text-7xl mb-4'>Memoir </h1>
+        <p className='my-4'> Slogan </p>
+        <button className='my-8 btn glass px-10 py-1'
+        onClick={handleClick}> Sign Up. </button>
+      </div>
+      
+
     </div>
   )
 }
