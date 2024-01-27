21eb180f4ba06d2deedddf5e66c1e9d2fd172bb2
xpbowler <rnquan30@gmail.com>
2024-01-27 14:20:45 -0500

    Update dashboard.jsx

--- front/memoir/src/Pages/dashboard.jsx
+++ front/memoir/src/Pages/dashboard.jsx
@@ -1,11 +1,8 @@
+import React, { useEffect, useState} from 'react'
 import { useAuth0 } from '@auth0/auth0-react';
 import Clouds from '../Components/clouds';
 import Friends from '../Components/friends';
-
 import { NavLink } from 'react-router-dom';
-
-
-
 
 const Dashboard = () => {
 
@@ -13,19 +10,14 @@ const Dashboard = () => {
   const [clouds, setClouds] = useState(false)
 
   return (
-
-    <div className='h-screen w-screen bg-cover'>
-      <div className='flex space-x-8 justify-center mt-4'>
+    <div className='h-screen w-screen bg-cover bg-[#E8E4FF]'>
+      <div className='flex space-x-8 mt-4 bg-nostalgia flex-row content-center'>
+        <h1 className='mt-3 ml-5 font-bold text-4xl mb-4 text-white font-title'>memoir </h1>
         <button onClick={()=>setClouds(false)}> Friends </button>
         <button onClick={()=>setClouds(true)}> Clouds </button>
       </div>
-      <NavLink to="/profile" className="absolute top-4 right-5"> My Profile (img later) </NavLink> 
+      <NavLink to="/profile" className="absolute mt-3 size-12 bg-stone-300 rounded-full top-4 right-5"></NavLink> 
       {clouds ? <Clouds/> : <Friends/>}
-
-
-
-
-
     </div>
   )
 }
