4cff5bc1eaf7194bb64ccd77434aa8e6beb5cdca
xpbowler <rnquan30@gmail.com>
2024-01-27 09:43:17 -0500

    clustering birch algorithm

---
+++ backend/main.py
@@ -0,0 +1,11 @@
+# create a graph representation (in a JSON?)
+# render said json on the frontend
+# making accounts??
+# Pages:
+#     profile
+#     cloud
+#     feed
+
+# group posts into a bubble, and group them into clouds?
+# to determine if same bubble do vector embbedding and difference needs to be small enuf
+# 
