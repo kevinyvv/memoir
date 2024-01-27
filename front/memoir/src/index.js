9682ad35970a4de63b34401000f2c732275b6b91
Kevin Wu <kevin31422005@gmail.com>
2024-01-27 09:47:58 -0500

    Update index.js
    fixed route

--- front/memoir/src/index.js
+++ front/memoir/src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Auth0Provider } from '@auth0/auth0-react';
+
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <BrowserRouter> 
     <React.StrictMode>
       <Auth0Provider
       domain={domain}
@@ -20,6 +23,7 @@ root.render(
         <App />
       </Auth0Provider>
     </React.StrictMode>
+    </BrowserRouter>
 
 );
 
